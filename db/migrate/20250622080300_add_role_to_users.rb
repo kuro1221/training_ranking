3681f758a18605fa1001d8885b0b31679e2ceb84
kuro1221 <84017305+kuro1221@users.noreply.github.com>
2025-07-07 07:56:39 +0900

    Remove admin migration

--- db/migrate/20250622080300_add_role_to_users.rb
+++ db/migrate/20250622080300_add_role_to_users.rb
@@ -1,6 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.2]
   def change
-    remove_column :users, :admin, :boolean
     add_column :users, :role, :integer, null: false, default: 1
   end
 end
